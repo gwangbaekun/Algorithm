9be0bafd32e8e4250345ab575f4fa39bfef23aaa
gwangbaekun <gwangbaekun@gmail.com>
2022-06-07 22:04:22 +0900

    단어의 개수

--- python/test.py
+++ python/test.py
@@ -2,30 +2,6 @@ import sys
 
 # sys.stdin = open("python.txt", "r")
 
-word = sys.stdin.readline().strip()
+sentence = sys.stdin.readline()
 
-upWord = word.upper()
-wordSet = list(set(upWord))
-
-countlist = []
-for char in wordSet:
-    count = 0
-    for i in upWord:
-        if char == i:
-            count += 1
-    countlist.append(count)
-
-def max_num(list):
-    arr = []
-    for i in range(len(list)):
-        if list[i] == max(list):
-            arr.append(i)
-    if len(arr) == 1:
-        return arr[0]
-    else:
-        return "?"
-
-if max_num(countlist) == '?':
-    print("?")
-else:
-    print(wordSet[max_num(countlist)])
+print(len(sentence.split()))
