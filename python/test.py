e39ba24de6bc1e33fbd0763013da64aaf7ed56bf
gwangbaekun <gwangbaekun@gmail.com>
2022-06-07 22:21:33 +0900

    상수

--- python/test.py
+++ python/test.py
@@ -2,6 +2,12 @@ import sys
 
 # sys.stdin = open("python.txt", "r")
 
-sentence = sys.stdin.readline()
+N, M = sys.stdin.readline().split()
 
-print(len(sentence.split()))
+def opposite(num):
+    return int(num[0]) + int(num[1]) * 10 + int(num[2]) * 100
+
+if opposite(N) > opposite(M):
+    print(opposite(N))
+else:
+    print(opposite(M))
