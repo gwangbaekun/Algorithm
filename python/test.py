4bf19dc1404c74acef71b0e70e9518e6bdd72775
gwangbaekun <gwangbaekun@gmail.com>
2022-06-13 19:20:12 +0900

    분수찾기

--- python/test.py
+++ python/test.py
@@ -1,14 +1,23 @@
 import sys
 
 # sys.stdin = open("python.txt", "r")
-
 N = int(sys.stdin.readline())
 
-room = 1
-count = 1
-while True:
-    if (N <= room):
-        print(count)
-        break
-    room += count * 6
-    count += 1
+# 분자: 1 / 1, 2 / 3, 2, 1 / 1, 2, 3, 4
+# 분모: 1 / 2, 1 / 1, 2, 3 / 4, 3, 2, 1
+
+roomNum = 0
+rooms = 0
+while N > rooms:
+    roomNum += 1
+    rooms += roomNum
+    if roomNum % 2 == 1:
+        top = rooms + 1 - N
+        bottom = roomNum + 1 - top
+    else:
+        bottom = rooms + 1 - N
+        top = roomNum + 1 - bottom
+
+
+print(str(top) + "/" + str(bottom))
+
