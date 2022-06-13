f5db3a2bfd2879b40dae53ebf7191aa0a1945f00
gwangbaekun <gwangbaekun@gmail.com>
2022-06-13 09:10:28 +0900

    벌집

--- python/test.py
+++ python/test.py
@@ -2,9 +2,13 @@ import sys
 
 # sys.stdin = open("python.txt", "r")
 
-A, B, C = map(int, sys.stdin.readline().strip().split())
-count = 0
-if B >= C:
-    print(-1)
-else:
-    print(A // (C - B) + 1)
+N = int(sys.stdin.readline())
+
+room = 1
+count = 1
+while True:
+    if (N <= room):
+        print(count)
+        break
+    room += count * 6
+    count += 1
