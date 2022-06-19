1289143821adddf9a1d6a301c69d16dea9be8f41
gwangbaekun <gwangbaekun@gmail.com>
2022-06-19 21:50:15 +0900

    베르트랑 공준

--- python/test.py
+++ python/test.py
@@ -3,29 +3,30 @@ import math
 import time
 
 # 주석------------
-# sys.stdin = open('python.txt', 'r')
-# start = time.time()
+sys.stdin = open('python.txt', 'r')
+start = time.time()
 #----------------
 
-case = int(sys.stdin.readline())
-
-def isPrime(x):
+def is_prime(x):
+    if x == 1:
+        return False
     for i in range(2, int(math.sqrt(x)) + 1):
         if x % i == 0:
             return False
     return True
 
-for _ in range(case):
-    n = int(sys.stdin.readline())
-    a, b = n//2, n//2
-    while a > 0:
-        if isPrime(a) and isPrime(b):
-            print(a, b)
-            break
-        a -= 1
-        b += 1
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    count = 0
+    for i in range(n + 1, 2 * n + 1):
+        if is_prime(i):
+            count += 1
+    print(count)
+
 
 # 주석------------
-# end = time.time()
-# print(f"{end - start:.5f} sec")
+end = time.time()
+print(f"{end - start:.5f} sec")
 #----------------
