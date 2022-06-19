5322d0df098348095b7dcb3764a49d2043f37630
gwangbaekun <gwangbaekun@gmail.com>
2022-06-19 20:16:44 +0900

    소인수분해

--- python/test.py
+++ python/test.py
@@ -1,31 +1,26 @@
 import sys
+import math
 import time
 
 # 주석------------
-sys.stdin = open('python.txt', 'r')
-start = time.time()
+# sys.stdin = open('python.txt', 'r')
+# start = time.time()
 #----------------
 
-M = int(sys.stdin.readline())    
-N = int(sys.stdin.readline())    
+N = int(sys.stdin.readline())
+d = 2
+sqrt = int(math.sqrt(N))
 
-primeNums = []
-for x in range(M, N+1):
-    error = 0
-    if x > 1:
-        for i in range(2, x):
-            if x % i == 0:
-                error += 1
-                break
-        if error == 0:
-            primeNums.append(x)
-
-if len(primeNums) > 0:
-    print(sum(primeNums), primeNums[0], sep='\n')
-else:
-    print(-1)
+while d <= sqrt:
+    if N % d != 0:
+        d += 1
+    else:
+        print(d)
+        N //= d
+if N > 1:
+    print(N)
 
 # 주석------------
-end = time.time()
-print(f"{end - start:.5f} sec")
+# end = time.time()
+# print(f"{end - start:.5f} sec")
 #----------------
