047841ffafc602e103b167dc3b01a9591eb5fa0d
gwangbaekun <gwangbaekun@gmail.com>
2022-08-01 18:58:19 +0900

    하노이 탑 이동 순서

--- python/test.py
+++ python/test.py
@@ -2,28 +2,38 @@ import sys
 import time
 
 # 주석------------
-# sys.stdin = open('python.txt', 'r')
-# start = time.time()
+sys.stdin = open('python.txt', 'r')
+start = time.time()
 #----------------
 
-def reculsive_fn(n, x):
-    space = '____' * (n-x)
-    a = space + '\"재귀함수가 뭔가요?\"' + '\n' + space + '\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.' + '\n' + space + '마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.' + '\n' + space + '그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"'
-    a_prime = space + '\"재귀함수가 뭔가요?\"' + '\n' + space + "\"재귀함수는 자기 자신을 호출하는 함수라네\""
-    b = space + "라고 답변하였지."
+Num = sys.stdin.readline()
 
-    if x == 0:
-        return a_prime + '\n'+ b
-    if x >= 1:
-        return  a + '\n' + reculsive_fn(n, x-1) + '\n' + b
+def block_reculsive(N):
+    if (N == 1):
+        track = [[1, 3]]
+        return track
 
-N = int(input())
-k = N
+    track = block_reculsive(N - 1)
 
-print('어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.')
-print(reculsive_fn(N, k))
+    if (N % 2 == 1):
+        track.append([3,2])
+        track.append([1,2])
+        track.append([1,3])
+        return track
+
+    
+    if (N % 2 == 0):
+        track.append([2,3])
+        track.append([2,1])
+        track.append([1,3])
+        return track
+
+for block in block_reculsive(Num):
+    print(block)
+
+
 
 # 주석------------
-# end = time.time()
-# print(f"{end - start:.5f} sec")
+end = time.time()
+print(f"{end - start:.5f} sec")
 #----------------
