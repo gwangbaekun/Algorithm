1f82dee67e145d74e4f79c67daa95a43b4e2f843
gwangbaekun <gwangbaekun@gmail.com>
2022-08-15 22:10:48 +0900

    수 정렬하기3

--- python/test.py
+++ python/test.py
@@ -7,14 +7,19 @@ import sys
 #----------------
 
 N = int(sys.stdin.readline())
+
+check_list = [0] * 10001
+
 nums = []
 for i in range(N):
-    nums.append(int(sys.stdin.readline().strip()))
+    num = int(sys.stdin.readline().strip())
 
-nums.sort()
+    check_list[num] = check_list[num] + 1
 
-for i in nums:
-    print(i)
+for i in range(10001):
+    if check_list[i] != 0:
+        for j in range(check_list[i]):
+            print(i) 
 # 주석------------
 # print("time : ", time.time() - start)
 #----------------
