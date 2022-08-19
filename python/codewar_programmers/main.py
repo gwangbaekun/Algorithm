cfed3bda028b0518ec36aaa6faf5ecb8f605e13e
gwangbaekun <gwangbaekun@gmail.com>
2022-08-19 11:26:40 +0900

    [codewars] Car Number Plate Calculator

--- python/codewar_programmers/main.py
+++ python/codewar_programmers/main.py
@@ -1,11 +1,12 @@
-def dig_pow(n, p):
-    charNum = str(n)
-    output = 0
-    for charIndex in range(0,len(charNum)):
-        char = int(charNum[charIndex])
-        output += char ** (p + charIndex)
-    count = 0
-    if (int(output / n) == output / n):
-        return output / n
-    else:
-        return -1
+#letters: abcdefghijklmnopqrstuvwxyz
+def find_the_number_plate(customer_id):
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    divide = customer_id // 999
+    letter = letters[divide % 26] + letters[divide // 26 % 26] + letters[divide // (26 * 26) % 26]
+    number = customer_id % 999 + 1
+    if number < 10:
+        return(letter + '00' + str(number))
+    elif number < 100:
+        return(letter + '0' + str(number))
+    else: 
+        return(letter + str(number))
