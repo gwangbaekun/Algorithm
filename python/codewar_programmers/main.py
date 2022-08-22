c2817c8db607e20026b14aabf30e5107a6f39634
gwangbaekun <gwangbaekun@gmail.com>
2022-08-22 15:29:43 +0900

    [codewars] Begin your day with a challenge, but an easy one.

--- python/codewar_programmers/main.py
+++ python/codewar_programmers/main.py
@@ -1,12 +1,7 @@
-#letters: abcdefghijklmnopqrstuvwxyz
-def find_the_number_plate(customer_id):
-    letters = 'abcdefghijklmnopqrstuvwxyz'
-    divide = customer_id // 999
-    letter = letters[divide % 26] + letters[divide // 26 % 26] + letters[divide // (26 * 26) % 26]
-    number = customer_id % 999 + 1
-    if number < 10:
-        return(letter + '00' + str(number))
-    elif number < 100:
-        return(letter + '0' + str(number))
-    else: 
-        return(letter + str(number))
+def one_two_three(n):
+    # just do it!
+    if n == 0:
+        return [0, 0]
+    else:
+        return [int(str('9' * (n // 9)) + str(n % 9)) , int('1' * n)] if n % 9 != 0 else [int(str('9' * (n // 9))) , int('1' * n)]
+        
