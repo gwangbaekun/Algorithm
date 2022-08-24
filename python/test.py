931b2fe0a084e73703fac45592b3b74f142ab55b
gwangbaekun <gwangbaekun@gmail.com>
2022-08-24 18:18:01 +0900

    단어 정렬 naive

--- python/test.py
+++ python/test.py
@@ -8,16 +8,58 @@ import sys
 
 N = int(sys.stdin.readline())
 
-arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+words = [sys.stdin.readline().strip() for _ in range(N)]
 
-for i in arr:
-    i.reverse()
+new_words_list = []
+length_sort = []
 
-arr.sort()
+for i in words:
+    new_obj = {
+        'word': i,
+        'length': len(i)
+    }
+    new_words_list.append(new_obj)
+    length_sort.append(len(i))
 
-for i in arr:
-    i.reverse()
-    print(i[0], i[1])
+
+length_sort.sort()
+
+real_sorted_length_array = []
+
+for i in range(len(length_sort)):
+    if length_sort[i] in real_sorted_length_array:
+        pass
+    else:
+        real_sorted_length_array.append(length_sort[i])
+
+answer = []
+
+for i in real_sorted_length_array:
+    to_sort = []
+    for j in new_words_list:
+        if i == j['length']:
+            to_sort.append(j)
+
+    # work with sort
+    to_sort.sort(key=lambda x: x['word'])
+    for j in to_sort:
+        answer.append(j['word'])
+
+# print(answer)
+
+new_answer = []
+
+for i in answer:
+    if i in new_answer:
+        pass
+    else:
+        new_answer.append(i)
+
+for i in new_answer:
+    print(i)
+
+
+
 
 # 주석------------
 # print("time : ", time.time() - start)
