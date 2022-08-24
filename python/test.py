bae0d6ac867a46aa6ec20f76914bc889b2856564
gwangbaekun <gwangbaekun@gmail.com>
2022-08-24 18:32:03 +0900

    단어 정렬 professional

--- python/test.py
+++ python/test.py
@@ -2,65 +2,28 @@ import time
 import sys
 
 # 주석------------
-# sys.stdin = open('python.txt', 'r')
-# start = time.time()
+sys.stdin = open('python.txt', 'r')
+start = time.time()
 #----------------
 
-N = int(sys.stdin.readline())
 
-words = [sys.stdin.readline().strip() for _ in range(N)]
+n = int(input())
 
-new_words_list = []
-length_sort = []
+word = []
+for i in range(n):
+    word.append(input())
 
-for i in words:
-    new_obj = {
-        'word': i,
-        'length': len(i)
-    }
-    new_words_list.append(new_obj)
-    length_sort.append(len(i))
+set_word = list(set(word))
 
+sort_word = []
 
-length_sort.sort()
+for i in set_word:
+    sort_word.append((len(i), i))
 
-real_sorted_length_array = []
+result = sorted(sort_word)
 
-for i in range(len(length_sort)):
-    if length_sort[i] in real_sorted_length_array:
-        pass
-    else:
-        real_sorted_length_array.append(length_sort[i])
-
-answer = []
-
-for i in real_sorted_length_array:
-    to_sort = []
-    for j in new_words_list:
-        if i == j['length']:
-            to_sort.append(j)
-
-    # work with sort
-    to_sort.sort(key=lambda x: x['word'])
-    for j in to_sort:
-        answer.append(j['word'])
-
-# print(answer)
-
-new_answer = []
-
-for i in answer:
-    if i in new_answer:
-        pass
-    else:
-        new_answer.append(i)
-
-for i in new_answer:
-    print(i)
-
-
-
+print(result)
 
 # 주석------------
-# print("time : ", time.time() - start)
+print("time : ", time.time() - start)
 #----------------
