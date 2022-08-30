d55eeff74de15b70fc2afe79503cde96bf1faffc
gwangbaekun <gwangbaekun@gmail.com>
2022-08-30 18:26:13 +0900

    숫자 카드

--- python/test.py
+++ python/test.py
@@ -7,13 +7,28 @@ import sys
 #----------------
 
 N = int(sys.stdin.readline())
+have_list = list(map(int, sys.stdin.readline().split(" ")))
+M = int(sys.stdin.readline())
+check_list = list(map(int, sys.stdin.readline().split(" ")))
 
-to_sort = [list(sys.stdin.readline().split()) for _ in range(N)]
+have_list.sort()
 
-to_sort.sort(key=lambda x: int(x[0]))
+def binary_tree(start, end, list, element):
+    while start <= end:
+        mean = (start + end) // 2
+        if element == list[mean]:
+            return mean
+        elif element < list[mean]:
+            end = mean - 1
+        else:
+            start = mean + 1
+    return None
 
-for i in to_sort:
-    print(" ".join(i))
+for i in range(M):
+    if binary_tree(0, N-1, have_list, check_list[i]) is None:
+        print(0, end=" ")
+    else:
+        print(1, end=" ")
 
 # 주석------------
 # print("time : ", time.time() - start)
