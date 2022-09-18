d5e684fe8c08d1e28fb39a027ec668a1bff7d7b3
Jeyeol BAEK <home@Jeyeols-MacBook-Pro.local>
2022-09-18 23:13:52 +0900

    네번째점

--- python/test.py
+++ python/test.py
@@ -1,13 +1,32 @@
 import time
-import sys
 # 주석------------
 sys.stdin = open('python.txt', 'r')
 start = time.time()
 #----------------
 
-x, y, w, h = map(int, input().split())
+import sys
+# C = [list(map(int, sys.stdin.readline().split(" "))) for _ in range(3)]
+dictX = {}
+dictY = {}
+for i in range(3):
+    x, y = map(int, sys.stdin.readline().split(" "))
+    if x in dictX:
+        dictX[x] += 1
+    else:
+        dictX[x] = 1
+    
+    if y in dictY:
+        dictY[y] += 1
+    else:
+        dictY[y] = 1
 
-print(min(x, y, w-x, h-y))
+for key in dictX:
+    if dictX[key] == 1:
+        print(key, end=" ")
+
+for key in dictY:
+    if dictY[key] == 1:
+        print(key)
 
 # 주석------------
 print("time : ", time.time() - start)
