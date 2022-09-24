6152b429a79f4c2a1f70452e2fd2641a16ce069c
Jeyeol BAEK <gwangbaekun@gmail.com>
2022-09-24 22:36:46 +0900

    택시 기하학

--- python/test.py
+++ python/test.py
@@ -1,39 +1,16 @@
 import sys
 import time
+import math
 # 주석------------
-sys.stdin = open('python.txt', 'r')
-start = time.time()
+# sys.stdin = open('python.txt', 'r')
+# start = time.time()
 #----------------
 
 N = int(sys.stdin.readline())
-d = []
-for _ in range(6):
-    a, b = map(int, sys.stdin.readline().split(" "))
-    d.append([a, b])
 
-max_a = 0
-max_a_index = -1
-
-for i in range(6):
-    if d[i][1] > max_a:
-        max_a = d[i][1]
-        max_a_index = i
-
-side = None
-
-if d[(max_a_index + 1) % 6][1] > d[max_a_index - 1][1]:
-    side = d[(max_a_index + 1) % 6][1]
-    empty_a = d[max_a_index - 2][1]
-    empty_b = d[max_a_index - 3][1]
-    print(N * (side * max_a - empty_a * empty_b))
-else:
-    side = d[max_a_index - 1][1]
-    empty_a = d[(max_a_index + 2) % 6][1]
-    empty_b = d[(max_a_index + 3) % 6][1]
-    print(N * (side * max_a - empty_a * empty_b))
-
-
+print(round(math.pi * N * N, 6))
+print("{:.6f}".format( 2 * N * N))
 
 # 주석------------
-print("time : ", time.time() - start)
+# print("time : ", time.time() - start)
 #----------------
