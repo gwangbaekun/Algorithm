01ff8e5770863a933d0f24d146e3ec28b93aadc2
Jeyeol BAEK <gwangbaekun@gmail.com>
2022-09-27 22:42:02 +0900

    배수와 약수

--- python/test.py
+++ python/test.py
@@ -1,26 +1,21 @@
 import sys
 import time
 # 주석------------
-sys.stdin = open('python.txt', 'r')
-start = time.time()
+# sys.stdin = open('python.txt', 'r')
+# start = time.time()
 #----------------
 
-W,H,X,Y,P = list(map(int ,sys.stdin.readline().split(" ")))
-
-count = 0
-
-for _ in range(P):
-    a, b = list(map(int, sys.stdin.readline().split(" ")))
-    # in square ?
-    if a >= X and a <= X+W and b >= Y and b <= Y+H:
-        count += 1
-    # X, Y+H/2
-    elif (X - a) ** 2 + (Y + H/2 - b) ** 2 <= (H/2) ** 2:
-        count += 1
-    elif (X + W - a) ** 2 + (Y + H/2 - b) ** 2 <= (H/2) ** 2:
-        count += 1
-
-print(count)
+while True:
+    a, b = map(int, sys.stdin.readline().split())
+    if (a == 0 and b == 0):
+        break
+    if (b % a == 0):
+        print('factor')
+    elif (a % b == 0):
+        print('multiple')
+    else:
+        print('neither')
+   
 # 주석------------
-print("time : ", time.time() - start)
+# print("time : ", time.time() - start)
 # ----------------
