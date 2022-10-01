649d70d3f88c4b9301616df6522bed2742e391c6
Jeyeol BAEK <gwangbaekun@gmail.com>
2022-10-01 22:47:21 +0900

    검문 **

--- python/test.py
+++ python/test.py
@@ -1,19 +1,37 @@
 import sys
 import time
+from math import gcd
 # 주석------------
 # sys.stdin = open('python.txt', 'r')
 # start = time.time()
 #----------------
 
 N = int(sys.stdin.readline())
+arr = []
+to_gcd = 0
 
-def gcd(a, b):
-    return b > 0 and gcd(b, a % b) or a
+for i in range(N):
+    arr.append(int(sys.stdin.readline()))
+    if i == 1:
+        to_gcd = abs(arr[i] - arr[0])
+    to_gcd = gcd(abs(arr[i] - arr[i - 1]), to_gcd)
 
-for _ in range(N):
-    a, b = map(int, sys.stdin.readline().split(" "))
-    print(a * b // gcd(a, b))
+gcd_squ = int(to_gcd ** 0.5)
 
+answer = []
+
+for i in range(2, gcd_squ + 1):
+    if to_gcd % i == 0:
+        answer.append(i)
+        answer.append(to_gcd // i)
+answer.append(to_gcd)
+answer = list(set(answer))
+
+answer.sort()
+
+for i in answer:
+    print(i, end=" ")
+    
 # 주석------------
 # print("time : ", time.time() - start)
 # ----------------
