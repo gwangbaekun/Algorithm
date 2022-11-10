f77aac8f77ab11c3e61b2d77605c1a46a83417a5
Jeyeol BAEK <gwangbaekun@gmail.com>
2022-11-10 21:18:58 +0900

    패션왕 신혜빈

--- python/test.py
+++ python/test.py
@@ -7,16 +7,19 @@ import time
 
 N = int(sys.stdin.readline())
 
-cases = [list(map(int, sys.stdin.readline().split(" "))) for _ in range(N)]
-
-def factorial(N):
-    if N == 0:
-        return 1
-    return N * factorial(N-1)
-
-for i in cases:
-    N, M = i[0], i[1]
-    print(factorial(M) // factorial(M-N) // factorial(N))
+for _ in range(N):
+    number_of_clothes = int(sys.stdin.readline())
+    clothes = {}
+    for _ in range(number_of_clothes):
+        input = (sys.stdin.readline().split(" "))
+        if (input[1].strip() in clothes):
+            clothes[input[1].strip()] += 1
+        else:
+            clothes[input[1].strip()] = 1
+    count = 1
+    for i in clothes.values():
+        count *= i + 1
+    print(count - 1)
 
 # 주석------------
 # print("time : ", time.time() - start)
