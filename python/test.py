a495bbf13d3b25a7c1e24ca58c07c098e1889b85
Jeyeol BAEK <gwangbaekun@gmail.com>
2022-11-15 20:39:35 +0900

    팩토리얼 0의 개수

--- python/test.py
+++ python/test.py
@@ -7,19 +7,7 @@ import time
 
 N = int(sys.stdin.readline())
 
-for _ in range(N):
-    number_of_clothes = int(sys.stdin.readline())
-    clothes = {}
-    for _ in range(number_of_clothes):
-        input = (sys.stdin.readline().split(" "))
-        if (input[1].strip() in clothes):
-            clothes[input[1].strip()] += 1
-        else:
-            clothes[input[1].strip()] = 1
-    count = 1
-    for i in clothes.values():
-        count *= i + 1
-    print(count - 1)
+print(N // 5 + N // 25 + N // 125)
 
 # 주석------------
 # print("time : ", time.time() - start)
