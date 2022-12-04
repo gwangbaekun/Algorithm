0da5e603f7884bb1d9b5c80e5cc7c36aa784acad
Jeyeol BAEK <gwangbaekun@gmail.com>
2022-12-05 01:47:50 +0900

    N과 M(1)

--- python/test.py
+++ python/test.py
@@ -1,28 +1,27 @@
 import sys
 import time
+
 # 주석------------
-# sys.stdin = open('python.txt', 'r')
-# start = time.time()
+sys.stdin = open('python.txt', 'r')
+start = time.time()
 #----------------
 
-N, M = map(int, sys.stdin.readline().split(" "))
+N, M = map(int ,sys.stdin.readline().split(" "))
 
-def counter(n, variable):
-    count = 1
-    divided_by_viriable = 0
-    while n >= variable:
-        if n // variable ** count == 0:
-            break
-        else:
-            divided_by_viriable += (n // variable ** count)
-        count += 1
-    return divided_by_viriable
+s = []
 
-fives = counter(N, 2) - counter(M, 2) - counter(N-M, 2)
-twos = counter(N, 5) - counter(M, 5) - counter(N-M, 5)
+def dfs():
+    if (len(s) == M):
+        print(" ".join(map(str, s)))
+        return
+    for i in range(1, N+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
 
-print(min(fives, twos))
+dfs()
 
 # 주석------------
-# print("time : ", time.time() - start)
-# ----------------
+print("time : ", time.time() - start)
+# ---------------
