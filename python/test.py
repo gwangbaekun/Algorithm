836b8078199e639d2dd2cb072e1dc58514868abf
Jeyeol BAEK <gwangbaekun@gmail.com>
2023-01-05 21:32:44 +0900

    연산자 끼워넣기

--- python/test.py
+++ python/test.py
@@ -3,65 +3,59 @@ import time
 import copy
 
 # 주석------------
-# sys.stdin = open('python.txt', 'r')
-# start = time.time()
+sys.stdin = open('python.txt', 'r')
+start = time.time()
 #----------------
 
-input_row = []
-            
-for _ in range(9):
-    input_row.append(list(map(int, sys.stdin.readline().split(" "))))
+N = int(sys.stdin.readline())
 
-def find_empty(board):
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 0:
-                return (i, j)
+sequence = list(map(int, sys.stdin.readline().split(" ")))
 
-def is_valid(board, num, pos):
+operators = list(map(int, sys.stdin.readline().split(" ")))
 
-    for i in range(len(board)):
-        if board[pos[0]][i] == num and pos[1] != i:
-            return False
-    
-    for i in range(len(board)):
-        if board[i][pos[1]] == num and pos[0] != i:
-            return False
+max_result = -1e9
 
-    box_x = pos[0] // 3
-    box_y = pos[1] // 3
+min_result = 1e9
 
-    for i in range(box_x * 3, box_x * 3 + 3):
-        for j in range(box_y * 3, box_y * 3 + 3):
-            if board[i][j] == num and (i, j) != pos:
-                return False
-
-    return True
-
-def sudoku(board):
-    empty = find_empty(board)
-    if not empty:
-        return True
+def cal(index, depth, cal_status):
+    if index == 0:
+         cal_status += sequence[depth]
+    elif index == 1:
+         cal_status -= sequence[depth]
+    elif index == 2:
+        cal_status *= sequence[depth]
     else:
-        row, col = empty
+        cal_status = int(cal_status / sequence[depth])
 
-    for num in range(1, 10):
-        if is_valid(board, num, (row, col)):
-            board[row][col] = num
+    return cal_status
 
-            if sudoku(board):
-                return True
-            
-            board[row][col] = 0
+def dfs(depth, operators, result):
+    global max_result
+    global min_result
+    if depth == N:
+        max_result = max(max_result, result)
+        min_result = min(min_result, result)
+        return
+    
+    else:
+        if operators[2] > 0:
+            new_operators = [operators[0], operators[1], operators[2] - 1, operators[3]]
+            dfs(depth + 1 , new_operators, cal(2, depth, result))
+        if operators[3] > 0:
+            new_operators = [operators[0], operators[1], operators[2], operators[3] - 1]
+            dfs(depth + 1, new_operators, cal(3, depth, result))
+        if operators[0] > 0:
+            new_operators = [operators[0] - 1, operators[1], operators[2], operators[3]]
+            dfs(depth + 1 , new_operators, cal(0, depth, result))
+        if operators[1] > 0:
+            new_operators = [operators[0], operators[1] - 1, operators[2], operators[3]]
+            dfs(depth + 1 , new_operators, cal(1, depth, result))
 
-    return False
+dfs(1, operators, sequence[0])
 
-
-sudoku(input_row)
-
-for elem in input_row:
-    print(" ".join(map(str, elem)))
+print(max_result)
+print(min_result)
 
 # 주석------------
-# print("time : ", time.time() - start)
+print("time : ", time.time() - start)
 # ---------------
