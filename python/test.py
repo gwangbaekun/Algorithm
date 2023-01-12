2e42d6b1460773a3ec2abc13057bb5e0e7913f6c
Jeyeol BAEK <gwangbaekun@gmail.com>
2023-01-12 20:38:30 +0900

    *dp*신나는 함수 실행

--- python/test.py
+++ python/test.py
@@ -7,54 +7,32 @@ sys.stdin = open('python.txt', 'r')
 start = time.time()
 #----------------
 
-N = int(sys.stdin.readline())
-
-sequence = list(map(int, sys.stdin.readline().split(" ")))
-
-operators = list(map(int, sys.stdin.readline().split(" ")))
-
-max_result = -1e9
-
-min_result = 1e9
-
-def cal(index, depth, cal_status):
-    if index == 0:
-         cal_status += sequence[depth]
-    elif index == 1:
-         cal_status -= sequence[depth]
-    elif index == 2:
-        cal_status *= sequence[depth]
-    else:
-        cal_status = int(cal_status / sequence[depth])
-
-    return cal_status
-
-def dfs(depth, operators, result):
-    global max_result
-    global min_result
-    if depth == N:
-        max_result = max(max_result, result)
-        min_result = min(min_result, result)
-        return
+def w(a, b, c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
     
-    else:
-        if operators[2] > 0:
-            new_operators = [operators[0], operators[1], operators[2] - 1, operators[3]]
-            dfs(depth + 1 , new_operators, cal(2, depth, result))
-        if operators[3] > 0:
-            new_operators = [operators[0], operators[1], operators[2], operators[3] - 1]
-            dfs(depth + 1, new_operators, cal(3, depth, result))
-        if operators[0] > 0:
-            new_operators = [operators[0] - 1, operators[1], operators[2], operators[3]]
-            dfs(depth + 1 , new_operators, cal(0, depth, result))
-        if operators[1] > 0:
-            new_operators = [operators[0], operators[1] - 1, operators[2], operators[3]]
-            dfs(depth + 1 , new_operators, cal(1, depth, result))
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
 
-dfs(1, operators, sequence[0])
+    if dp[a][b][c]:
+        return dp[a][b][c]
 
-print(max_result)
-print(min_result)
+    if a < b and b < c:
+        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+
+    dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+
+    return dp[a][b][c]
+
+dp = [[[0]*(21) for _ in range(21)] for _ in range(21)]
+
+while True:
+    a, b, c = map(int, sys.stdin.readline().split(" "))
+    if a == -1 and b == -1 and c == -1:
+        break
+    print(f'w({a}, {b}, {c}) =', w(a,b,c))
+
+# print(dp)
 
 # 주석------------
 print("time : ", time.time() - start)
