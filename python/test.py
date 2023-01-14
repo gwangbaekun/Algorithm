e289c0a0f8f048402940b3107246049eae3d4c45
Jeyeol BAEK <gwangbaekun@gmail.com>
2023-01-14 18:02:24 +0900

    *01타일

--- python/test.py
+++ python/test.py
@@ -7,32 +7,16 @@ sys.stdin = open('python.txt', 'r')
 start = time.time()
 #----------------
 
-def w(a, b, c):
-    if a <= 0 or b <= 0 or c <= 0:
-        return 1
-    
-    if a > 20 or b > 20 or c > 20:
-        return w(20, 20, 20)
+N = int(sys.stdin.readline())
 
-    if dp[a][b][c]:
-        return dp[a][b][c]
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
 
-    if a < b and b < c:
-        dp[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+for k in range(3, N+1):
+    dp[k] = (dp[k-1] + dp[k-2]) % 15746
 
-    dp[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
-
-    return dp[a][b][c]
-
-dp = [[[0]*(21) for _ in range(21)] for _ in range(21)]
-
-while True:
-    a, b, c = map(int, sys.stdin.readline().split(" "))
-    if a == -1 and b == -1 and c == -1:
-        break
-    print(f'w({a}, {b}, {c}) =', w(a,b,c))
-
-# print(dp)
+print(dp[N])
 
 # 주석------------
 print("time : ", time.time() - start)
