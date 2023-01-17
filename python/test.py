30ff7d6ae5f14a96a582b0d4c813acb8d1bc9fa4
Jeyeol BAEK <gwangbaekun@gmail.com>
2023-01-17 19:03:11 +0900

    파도반 수열

--- python/test.py
+++ python/test.py
@@ -2,21 +2,22 @@ import sys
 import time
 import copy
 
+
 # 주석------------
 sys.stdin = open('python.txt', 'r')
 start = time.time()
 #----------------
+r = sys.stdin.readline
+N = int(r())
 
-N = int(sys.stdin.readline())
+for _ in range(N):
+    n = int(r())
+    seq = [1,1,1,2,2]
 
-dp = [0] * 1000001
-dp[1] = 1
-dp[2] = 2
+    for i in range(5, n):
+        seq.append(seq[i - 1] + seq[i - 5])
 
-for k in range(3, N+1):
-    dp[k] = (dp[k-1] + dp[k-2]) % 15746
-
-print(dp[N])
+    print(seq[n-1])
 
 # 주석------------
 print("time : ", time.time() - start)
