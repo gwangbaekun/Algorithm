d7fdffcafbbd2ade80745c0892d48f341110fe1a
Jeyeol BAEK <gwangbaekun@gmail.com>
2023-01-23 21:15:44 +0900

    수열

--- python/test.py
+++ python/test.py
@@ -2,25 +2,20 @@ import sys
 import time
 
 # 주석------------
-sys.stdin = open('python.txt', 'r')
-start = time.time()
+# sys.stdin = open('python.txt', 'r')
+# start = time.time()
 #----------------
 
 A, B = map(int, sys.stdin.readline().split(" "))
 arr = list(map(int, sys.stdin.readline().split(" ")))
-prefixsum = []
-total = 0
-for i in range(len(arr)):
-    total += arr[i]
-    prefixsum.append(total)
 
-for _ in range(B):
-    N, M = map(int, sys.stdin.readline().split(" "))
-    if N == 1:
-        print(prefixsum[M - 1])
-    else:
-        print(prefixsum[M - 1] - prefixsum[N - 2])
+total = []
+total.append(sum(arr[:B]))
 
+for i in range(A - B):
+    total.append(total[i] - arr[i] + arr[B + i])
+    
+print(max(total))
 # 주석------------
-print(f"time : {time.time() - start:.5f} sec")
+# print(f"time : {time.time() - start:.5f} sec")
 # ---------------
