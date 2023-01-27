0d64a482fb9bba65da2d9d6c6a3a6c5008e639ff
Jeyeol BAEK <gwangbaekun@gmail.com>
2023-01-27 20:17:44 +0900

    괄호

--- python/test.py
+++ python/test.py
@@ -6,27 +6,27 @@ sys.stdin = open('python.txt', 'r')
 start = time.time()
 #----------------
 
-N, M = map(int, sys.stdin.readline().split(" "))
-arr = []
-sum_arr = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
-
+N = int(sys.stdin.readline())
 
 for _ in range(N):
-    arr.append(list(map(int, sys.stdin.readline().split(" "))))
-
-sum_arr[1][1] = arr[0][0]
-
-def procession_sum(arr):
-    for i in range(1, len(arr) + 1):
-        for j in range(1, len(arr) + 1):
-            sum_arr[i][j] = sum_arr[i - 1][j] + sum_arr[i][j - 1] + arr[i - 1][j - 1] - sum_arr[i - 1][j - 1]
-    return sum_arr
-
-sum_arr = procession_sum(arr)
-
-for _ in range(M):
-    x1, y1, x2, y2 = map(int, sys.stdin.readline().split(" "))
-    print(sum_arr[x2][y2] - sum_arr[x2][y1 - 1] - sum_arr[x1 - 1][y2] + sum_arr[x1 - 1][y1 - 1])
+    test = sys.stdin.readline().strip()
+    left = 0
+    right = 0
+    for elem in test:
+        if elem == '(':
+            left += 1
+        else:
+            right += 1
+        
+        if right > left:
+            break
+    
+    if right == left:
+        print('YES')
+    else:
+        print('NO')
+    
+    
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
