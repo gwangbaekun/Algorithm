2721634d7f26876e8fa3690066277c75d233fb14
Jeyeol BAEK <gwangbaekun@gmail.com>
2023-02-13 19:11:30 +0900

    *공유기 설치

--- python/test.py
+++ python/test.py
@@ -6,24 +6,30 @@ sys.stdin = open('python.txt', 'r')
 start = time.time()
 #----------------
 
-N, K = map(int, sys.stdin.readline().split(" "))
-arr = list(map(int, sys.stdin.readline().split(" ")))
+N, C = map(int, sys.stdin.readline().split(" "))
+arr = [int(sys.stdin.readline()) for _ in range(N)]
 
-start, end = max(arr) - K, max(arr)
+arr.sort()
+start = 1
+end = arr[-1] - arr[0]
 
 while start <= end:
     mid = (start + end) // 2
-    lo = 0
-    for elem in arr:
-        if elem > mid:
-            lo += elem - mid
+    cur = arr[0]
+    count = 1
 
-    if lo >= K:
+    for i in range(1, len(arr)):
+        if arr[i] >= cur + mid:
+            count += 1
+            cur = arr[i]
+
+    if count >= C:
         start = mid + 1
+        answer = mid
     else:
         end = mid - 1
 
-print(end)
+print(answer)
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
