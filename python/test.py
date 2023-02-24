0fa7afe1199bee9e6ac33d79a8e66a08c1af36e0
Jeyeol BAEK <gwangbaekun@gmail.com>
2023-02-24 12:24:58 +0900

    *K번쨰 수

--- python/test.py
+++ python/test.py
@@ -7,31 +7,25 @@ from collections import deque
 # start = time.time()
 #----------------
 
-N, C = map(int, sys.stdin.readline().split(" "))
-arr = []
-for _ in range(N):
-    arr.append(int(sys.stdin.readline()))
+N = int(sys.stdin.readline())
+k = int(sys.stdin.readline())
 
-arr.sort()
 start = 1
-end = arr[N - 1] - arr[0]
+end = k
 answer = 0
 
 while start <= end:
     mid = (start + end) // 2
-    cur = arr[0]
-    count = 1
+    count = 0
 
-    for i in range(1, N):
-        if arr[i] >= cur + mid:
-            count += 1
-            cur = arr[i]
-    
-    if count >= C:
-        start = mid + 1
+    for i in range(1, N+1):
+        count += min(mid // i, N)
+
+    if count >= k:
+        end = mid - 1
         answer = mid
     else:
-        end = mid - 1
+        start = mid + 1
 
 print(answer)
 
