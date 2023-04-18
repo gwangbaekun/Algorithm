a9e06c41aeac8ea94e77a680fc0a50efdb67d59e
gwangbaekun <gwangbaekun@gmail.com>
2023-04-19 02:33:31 +0900

    오큰수

--- python/test.py
+++ python/test.py
@@ -7,41 +7,21 @@ sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
 #----------------
 
-while True:
-    recs = list(map(int, sys.stdin.readline().split(" ")))
-    lenInput = recs[0]
-    recs = recs[1:]
-    if lenInput == 0:
-        break
+N = int(sys.stdin.readline())
+input = list(map(int, sys.stdin.readline().split(" ")))
 
-    stack = deque()
-    area = 0
+# 인덱스로 넣을것임
+stack = deque()
 
-    for i in range(lenInput):
-        while len(stack) != 0 and recs[stack[-1]] > recs[i]:
-            tmp = stack.pop()
-            
-            if len(stack) == 0:
-                width = i
-            else:
-                width = i - stack[-1] - 1
-            
-            area = max(area, width * recs[tmp])
+answer = [-1 for _ in range(N)]
 
-        stack.append(i)
+for i in range(N):
+    while len(stack) != 0 and input[stack[-1]] < input[i]:
+        decidedIndex = stack.pop()
+        answer[decidedIndex] = input[i]
+    stack.append(i)
 
-    while len(stack) != 0:
-        tmp = stack.pop()
-
-        if len(stack) == 0:
-            width = lenInput
-        else:
-            width = lenInput - stack[-1] - 1
-
-        area = max(area, width * recs[tmp])
-
-    print(area)
-            
+print(" ".join(map(str, answer)))
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
