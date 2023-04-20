3e01c3d7d2879516051f86bc6e067dcef07bc5fc
gwangbaekun <gwangbaekun@gmail.com>
2023-04-20 11:09:19 +0900

    오등큰수

--- python/test.py
+++ python/test.py
@@ -3,26 +3,37 @@ import time
 from collections import deque
 
 # 주석------------
-sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
-start = time.time()
+# sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
+# start = time.time()
 #----------------
 
 N = int(sys.stdin.readline())
-input = list(map(int, sys.stdin.readline().split(" ")))
+nums = list(map(int, sys.stdin.readline().split(" ")))
 
-# 인덱스로 넣을것임
 stack = deque()
-
+numMap = {}
+checkArr = [0 for _ in range(N)]
 answer = [-1 for _ in range(N)]
 
 for i in range(N):
-    while len(stack) != 0 and input[stack[-1]] < input[i]:
-        decidedIndex = stack.pop()
-        answer[decidedIndex] = input[i]
+    if nums[i] in numMap:
+        numMap[nums[i]] += 1
+    else:
+        numMap[nums[i]] = 1
+
+for i in range(N):
+    checkArr[i] = numMap[nums[i]]
+
+for i in range(N):
+    while len(stack) != 0 and checkArr[stack[-1]] < checkArr[i]:
+        tmp = stack.pop()
+
+        answer[tmp] = nums[i]
+
     stack.append(i)
 
 print(" ".join(map(str, answer)))
 
 # 주석------------
-print(f"time : {time.time() - start:.5f} sec")
+# print(f"time : {time.time() - start:.5f} sec")
 # ---------------
