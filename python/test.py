fa2fbab5a36fc9489e6fe21af2a0ce4ab18da9e3
gwangbaekun <gwangbaekun@gmail.com>
2023-04-28 03:18:08 +0900

    가장 긴 증가하는 부분 수열

--- python/test.py
+++ python/test.py
@@ -3,37 +3,23 @@ import time
 from collections import deque
 
 # 주석------------
-# sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
-# start = time.time()
+sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
+start = time.time()
 #----------------
 
 N = int(sys.stdin.readline())
-nums = list(map(int, sys.stdin.readline().split(" ")))
+A = list(map(int, sys.stdin.readline().split(" ")))
 
-stack = deque()
-numMap = {}
-checkArr = [0 for _ in range(N)]
-answer = [-1 for _ in range(N)]
+dp = [1 for i in range(N)]
 
 for i in range(N):
-    if nums[i] in numMap:
-        numMap[nums[i]] += 1
-    else:
-        numMap[nums[i]] = 1
+     for j in range(i):
+          if A[i] > A[j]:
+               dp[i] = max(dp[i], dp[j] + 1)
 
-for i in range(N):
-    checkArr[i] = numMap[nums[i]]
-
-for i in range(N):
-    while len(stack) != 0 and checkArr[stack[-1]] < checkArr[i]:
-        tmp = stack.pop()
-
-        answer[tmp] = nums[i]
-
-    stack.append(i)
-
-print(" ".join(map(str, answer)))
+print(max(dp))
 
 # 주석------------
-# print(f"time : {time.time() - start:.5f} sec")
+print(f"time : {time.time() - start:.5f} sec")
 # ---------------
+
