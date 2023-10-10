c987fa6a189793faf31cf56ca0636faf01fc2ca0
gwangbaekun <gwangbaekun@gmail.com>
2023-10-10 19:32:16 +0900

    행렬 덧셈

--- python/test.py
+++ python/test.py
@@ -2,35 +2,29 @@ import sys
 import time
 
 # 주석------------
-sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
-start = time.time()
+# sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
+# start = time.time()
 #----------------
 
-gradeTransformer = {
-   'A+': 4.5,
-   'A0': 4.0,
-   'B+': 3.5,
-   'B0': 3.0,
-   'C+': 2.5,
-   'C0': 2.0,
-   'D+': 1.5,
-   'D0': 1.0,
-   'F': 0,
-   'P': 0
-}
+N, M = map(int, sys.stdin.readline().split())
 
-grades = 0
-scores = 0
+A = []
+B = []
 
-for _ in range(20):
-    _, grade, score = sys.stdin.readline().strip().split(" ")
-    if score != 'P': 
-        grades += float(grade)
-        scores += gradeTransformer[score] * float(grade)
+for _ in range(N):
+    A.append(list(map(int, sys.stdin.readline().split(" "))))
 
-print('%.6f' % (scores / grades))
+for i in range(N):
+    B.append(list(map(int, sys.stdin.readline().split(" "))))
+
+for i in range(N):
+    for j in range(M):
+        A[i][j] += B[i][j]
+
+for i in range(N):
+    print(*A[i])
 
 # 주석------------
-print(f"time : {time.time() - start:.5f} sec")
+# print(f"time : {time.time() - start:.5f} sec")
 # ---------------
 
