2622e311105ab34de0641a67cd73493c9c6050df
gwangbaekun <gwangbaekun@gmail.com>
2023-10-11 21:34:09 +0900

    **색종이

--- python/test.py
+++ python/test.py
@@ -2,26 +2,21 @@ import sys
 import time
 
 # 주석------------
-sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
-start = time.time()
+# sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
+# start = time.time()
 #----------------
 
-strings = [list(sys.stdin.readline().rstrip()) for _ in range(5)]
-maxString = 0
+coordinates = [[0 for _ in range(100)] for _ in range(100)]
 
-for i in range(len(strings)):
-    if maxString < len(strings[i]):
-        maxString = len(strings[i])
+N = int(sys.stdin.readline())
+papers = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-for j in range(maxString):
-    for i in range(5):
-        if len(strings[i]) > j:
-            print(strings[i][j], end='')
-        # print(strings[i][j], end='')
+for paper in papers:
+    for i in range(paper[0], paper[0]+10):
+        for j in range(paper[1], paper[1]+10):
+            coordinates[i][j] = 1
 
-# for i in range(5):
-#     print(strings[i][1], end='')
-
+print(sum(sum(coordinates, [])))
 
 # 주석------------
 # print(f"time : {time.time() - start:.5f} sec")
