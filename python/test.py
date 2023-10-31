db29e7cfdf5af14af930ec3691c8e273affa9944
gwangbaekun <gwangbaekun@gmail.com>
2023-10-31 22:51:00 +0900

    분해합

--- python/test.py
+++ python/test.py
@@ -6,17 +6,21 @@ sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
 #----------------
 
-N, M = map(int, sys.stdin.readline().split())
-cards = list(map(int,sys.stdin.readline().split()))
-result = 0
-for i in range(N):
-    for j in range(i + 1, N):
-        for k in range(j + 1, N):
-            sum = cards[i] + cards[j] + cards[k]
-            if sum <= M:
-                result = max(result, sum)
+N = int(sys.stdin.readline())
 
-print(result)
+def get_number_sum(n):
+    sum = 0
+    for i in range(len(str(n))):
+        sum += int(str(n)[i])
+    return sum + n
+
+for i in range(N):
+    if get_number_sum(i) == N:
+        print(i)
+        break
+    if i == N-1:
+        print(0)
+
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
