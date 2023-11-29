bdeaddd68eaa0a65ee4d0563c3d65073ccc569ca
gwangbaekun <gwangbaekun@gmail.com>
2023-11-29 15:50:11 +0900

    체스판 다시 칠하기

--- python/test.py
+++ python/test.py
@@ -6,17 +6,45 @@ sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
 #----------------
 
-a,b,c,d,e,f = map(int, sys.stdin.readline().split())
+N, M = map(int, sys.stdin.readline().split())
+board = [sys.stdin.readline().rstrip() for _ in range(N)]
 
-if (a * e - d * b) == 0 or (b * d - e * a) == 0:
-    x = (a + b) // c
-    y = (a + b) // c
-else:
-    y = (c * d - f * a) // (b * d - e * a)
-    x = (c * e - f * b) // (a * e - d * b)
+bf_ans = 0
 
-print(x, y)
+result = []
 
+def check(i, j, board):
+    w_cnt = 0
+    b_cnt = 0
+    for x in range(i, i + 8):
+        for y in range(j, j + 8):
+            # w 로 시작했을 때
+            if (x + y) % 2 == 0:
+                if board[x][y] != 'W':
+                    w_cnt += 1
+            else:
+                if board[x][y] != 'B':
+                    w_cnt += 1
+
+            if (x + y) % 2 == 0:
+                if board[x][y] != 'B':
+                    b_cnt += 1
+            else:
+                if board[x][y] != 'W':
+                    b_cnt += 1
+
+    return min(w_cnt, b_cnt)
+
+#시작을 찾기
+for i in range(N - 7):
+    for j in range(M - 7):
+        ans = check(i, j, board)
+        if bf_ans == 0 or ans < bf_ans:
+            bf_ans = ans
+        result.append(check(i, j, board))
+        
+
+print(min(result), bf_ans)
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
