77f16752075bd6e02aa9d8ac6a32b94f69e4f082
gwangbaekun <gwangbaekun@gmail.com>
2023-11-30 16:01:47 +0900

    영화감독 숌

--- python/test.py
+++ python/test.py
@@ -6,47 +6,20 @@ sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
 #----------------
 
-N, M = map(int, sys.stdin.readline().split())
-board = [sys.stdin.readline().rstrip() for _ in range(N)]
+N = int(sys.stdin.readline())
 
-bf_ans = 0
+dume = []
+num = 0
 
-result = []
+while len(dume) < N:
+    if '666' in str(num):
+        dume.append(num)
+        num += 1
+    else:
+        num += 1
 
-def check(i, j, board):
-    w_cnt = 0
-    b_cnt = 0
-    for x in range(i, i + 8):
-        for y in range(j, j + 8):
-            # w 로 시작했을 때
-            if (x + y) % 2 == 0:
-                if board[x][y] != 'W':
-                    w_cnt += 1
-            else:
-                if board[x][y] != 'B':
-                    w_cnt += 1
-
-            if (x + y) % 2 == 0:
-                if board[x][y] != 'B':
-                    b_cnt += 1
-            else:
-                if board[x][y] != 'W':
-                    b_cnt += 1
-
-    return min(w_cnt, b_cnt)
-
-#시작을 찾기
-for i in range(N - 7):
-    for j in range(M - 7):
-        ans = check(i, j, board)
-        if bf_ans == 0 or ans < bf_ans:
-            bf_ans = ans
-        result.append(check(i, j, board))
-        
-
-print(min(result), bf_ans)
+print(dume[-1])
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
 # ---------------
-
