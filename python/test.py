01d3b3011b6ec9f94518466778d017f8040e496d
gwangbaekun <gwangbaekun@gmail.com>
2023-12-02 21:24:18 +0900

    골드바흐 파티션 *에라토스테네스의 체

--- python/test.py
+++ python/test.py
@@ -6,19 +6,28 @@ sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
 #----------------
 
-N = int(sys.stdin.readline())
-members = dict()
-for _ in range(N):
-    name, status = sys.stdin.readline().split()
-    if status == 'enter':
-        members[name] = status
-    else:
-        members.pop(name)
-        
-members = sorted(members.keys(), reverse=True)
+prime = []
+check = [0] * 1000001
+check[0] = 1
+check[1] = 1
 
-for member in members:
-    print(member)
+for i in range(2, 1000001):
+    if check[i] == 0:
+        prime.append(i)
+        for j in range(2*i, 1000001, i):
+            check[j] = 1
+
+N = int(sys.stdin.readline())
+
+for _ in range(N):
+    count = 0
+    n = int(sys.stdin.readline())
+    for i in prime:
+        if i > n:
+            break
+        if check[n - i] == 0 and i <= n - i:
+            count += 1
+    print(count)
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
