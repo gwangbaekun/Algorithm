ce362eab6f3dc312c957639a9bf55b2a44ebf832
gwangbaekun <gwangbaekun@gmail.com>
2023-12-05 22:43:02 +0900

    최소공배수

--- python/test.py
+++ python/test.py
@@ -6,28 +6,22 @@ sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
 #----------------
 
-prime = []
-check = [0] * 1000001
-check[0] = 1
-check[1] = 1
+N, M = map(int, sys.stdin.readline().split())
 
-for i in range(2, 1000001):
-    if check[i] == 0:
-        prime.append(i)
-        for j in range(2*i, 1000001, i):
-            check[j] = 1
+def gcd(a, b):
+    if a < b:
+        a, b = a, b
+    if b == 0:
+        return a
+    if a % b == 0:
+        return b
+    else:
+        return gcd(b, a % b)
+    
+def lcm(a, b):
+    return a * b // gcd(a, b)
 
-N = int(sys.stdin.readline())
-
-for _ in range(N):
-    count = 0
-    n = int(sys.stdin.readline())
-    for i in prime:
-        if i > n:
-            break
-        if check[n - i] == 0 and i <= n - i:
-            count += 1
-    print(count)
+print(lcm(a, b))
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
