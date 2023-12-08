afd40ef29b295784a13122bd628a53aab93e006d
gwangbaekun <gwangbaekun@gmail.com>
2023-12-08 23:59:03 +0900

    가로수

--- python/test.py
+++ python/test.py
@@ -1,34 +1,29 @@
 import sys
 import time
+from math import gcd
 
 # 주석------------
-# sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
-# start = time.time()
+sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
+start = time.time()
 #----------------
 
-A, B = map(int, sys.stdin.readline().split())
-C, D = map(int, sys.stdin.readline().split())
+N = int(sys.stdin.readline())
 
-def gcd(a, b):
-    if a < b:
-        a, b = b, a
-    if b == 0:
-        return a
-    return gcd(b, a%b)
+a = int(sys.stdin.readline())    
+arr = []
+for _ in range(1, N):
+    num = int(sys.stdin.readline())
+    arr.append(num - a)
+    a = num
+        
+g = arr[0]
+for i in range(1, len(arr)):
+    g = gcd(g, arr[i])
 
-def lcm(a, b):
-    return a * b // gcd(a, b)
-
-def solution(a, b, c, d):
-    lcmNum = lcm(b, d)
-    a = a * (lcmNum // b)
-    c = c * (lcmNum // d)
-    return a + c, lcmNum
-
-top, bottom = solution(A, B, C, D)
-
-print(top // gcd(top,bottom), bottom // gcd(top,bottom))
+result = 0
+for each in arr:
+    result += each // g - 1
 
 # 주석------------
-# print(f"time : {time.time() - start:.5f} sec")
+print(f"time : {time.time() - start:.5f} sec")
 # ---------------
