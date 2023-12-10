bcd6af3688924dd6ce53fa7ecf5577ac691c78d1
gwangbaekun <gwangbaekun@gmail.com>
2023-12-10 16:34:10 +0900

    창문 닫기

--- python/test.py
+++ python/test.py
@@ -7,22 +7,14 @@ import time
 #----------------
 
 N = int(sys.stdin.readline())
+count = 0
+check = 1
+while check ** 2 <= N:
+    check += 1
+    count += 1
 
-def isPrime(n):
-    if n == 0:
-        return False
-    if n == 1:
-        return False
-    for i in range(2, int(n **0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-     
-for _ in range(N):
-    n = int(sys.stdin.readline())
-    while not isPrime(n):
-        n += 1
-    print(n)
+print(count)
+
 # 주석------------
 # print(f"time : {time.time() - start:.5f} sec")
 # ---------------
