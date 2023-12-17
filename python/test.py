e3c1c244692eb5c5b961e42c60b9719082c83db4
gwangbaekun <gwangbaekun@gmail.com>
2023-12-17 17:17:00 +0900

    덱 2

--- python/test.py
+++ python/test.py
@@ -8,18 +8,41 @@ start = time.time()
 #----------------
 
 q = deque()
-l = []
-N, K = map(int, sys.stdin.readline().split())
+N = int(sys.stdin.readline())
 
-for i in range(N):
-    q.append(i + 1)
-
-while len(l) != N:
-    for i in range(K - 1):
-        q.append(q.popleft())
-    l.append(q.popleft())
-
-print('<' + ', '.join(str(x) for x in l) + '>')
+for _ in range(N):
+    cmd = list(map(int, sys.stdin.readline().split()))
+    if cmd[0] == 1:
+        q.appendleft(cmd[1])
+    elif cmd[0] == 2:
+        q.append(cmd[1])
+    elif cmd[0] == 3:
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == 4:
+        if q:
+            print(q.pop())
+        else:
+            print(-1)
+    elif cmd[0] == 5:
+        print(len(q))
+    elif cmd[0] == 6:
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 7:
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif cmd[0] == 8:
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
