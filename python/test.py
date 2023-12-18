c5f7e4d8dc58b7d1e328577dcf9993ae72ce9ad4
gwangbaekun <gwangbaekun@gmail.com>
2023-12-18 14:06:16 +0900

    풍선 터뜨리기

--- python/test.py
+++ python/test.py
@@ -7,42 +7,20 @@ sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
 #----------------
 
-q = deque()
 N = int(sys.stdin.readline())
+l = list(map(int, sys.stdin.readline().split()))
+
+q = deque([i for i in range(1, N + 1)])
+i = 0
 
 for _ in range(N):
-    cmd = list(map(int, sys.stdin.readline().split()))
-    if cmd[0] == 1:
-        q.appendleft(cmd[1])
-    elif cmd[0] == 2:
-        q.append(cmd[1])
-    elif cmd[0] == 3:
-        if q:
-            print(q.popleft())
-        else:
-            print(-1)
-    elif cmd[0] == 4:
-        if q:
-            print(q.pop())
-        else:
-            print(-1)
-    elif cmd[0] == 5:
-        print(len(q))
-    elif cmd[0] == 6:
-        if q:
-            print(0)
-        else:
-            print(1)
-    elif cmd[0] == 7:
-        if q:
-            print(q[0])
-        else:
-            print(-1)
-    elif cmd[0] == 8:
-        if q:
-            print(q[-1])
-        else:
-            print(-1)
+    pl = q.popleft()
+    print(pl, end=' ')
+    if l[pl - 1] > 0:
+        i = l[pl - 1] - 1
+    else:
+        i = l[pl - 1]
+    q.rotate(-i)
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
