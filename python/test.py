e4bf9e953f2f4506d36564122e162b2976813631
gwangbaekun <gwangbaekun@gmail.com>
2023-12-19 15:30:24 +0900

    queuestack

--- python/test.py
+++ python/test.py
@@ -8,19 +8,19 @@ start = time.time()
 #----------------
 
 N = int(sys.stdin.readline())
-l = list(map(int, sys.stdin.readline().split()))
+A = list(map(int, sys.stdin.readline().split()))
+B = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+C = list(map(int, sys.stdin.readline().split()))
 
-q = deque([i for i in range(1, N + 1)])
-i = 0
+queue = deque()
+for i in range(N):
+    if A[i] == 0:
+        queue.appendleft(B[i])
 
-for _ in range(N):
-    pl = q.popleft()
-    print(pl, end=' ')
-    if l[pl - 1] > 0:
-        i = l[pl - 1] - 1
-    else:
-        i = l[pl - 1]
-    q.rotate(-i)
+for i in range(M):
+    queue.append(C[i])
+    print(queue.popleft(), end=" ")
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
