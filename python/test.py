83aa6cdc8496a665ddb4be76e3c690f544cf3752
gwangbaekun <gwangbaekun@gmail.com>
2023-12-21 20:27:14 +0900

    재귀의 귀재

--- python/test.py
+++ python/test.py
@@ -5,18 +5,25 @@ from collections import deque
 # 주석------------
 sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
-#----------------
+#---------------- = 0
+
+def recursion(s, l, r, count):
+    if l >= r:
+        return 1, count + 1
+    elif s[l] != s[r]:
+        return 0, count + 1
+    else:
+        count += 1
+        return recursion(s, l+1, r-1, count)
+    
+def isPalindrome(s, count):
+    return recursion(s, 0, len(s)-1, count)
 
 N = int(sys.stdin.readline())
-answer = 1
 
-def factorial(n):
-    if n <= 1:
-        return 1
-    return n * factorial(n - 1)
-
-print(factorial(N))
-
+for _ in range(N):
+    s = sys.stdin.readline().strip()
+    print(isPalindrome(s, 0)[0], isPalindrome(s, 0)[1])
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
 # ---------------
