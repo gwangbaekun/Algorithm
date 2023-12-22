68426e02fdadcc295a01d437a130ad85c666a40d
gwangbaekun <gwangbaekun@gmail.com>
2023-12-22 15:02:39 +0900

    알고리즘 수업 - 병합 정렬 1

--- python/test.py
+++ python/test.py
@@ -5,25 +5,44 @@ from collections import deque
 # 주석------------
 sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
-#---------------- = 0
+#----------------
 
-def recursion(s, l, r, count):
-    if l >= r:
-        return 1, count + 1
-    elif s[l] != s[r]:
-        return 0, count + 1
-    else:
-        count += 1
-        return recursion(s, l+1, r-1, count)
+def merge_sort(arr):
+    if len(arr) <= 1 :
+        return arr
+    mid = (len(arr)+1)//2
+    low_arr = merge_sort(arr[:mid])
+    high_arr = merge_sort(arr[mid:])
     
-def isPalindrome(s, count):
-    return recursion(s, 0, len(s)-1, count)
+    sorted_arr= []
+    l=h=0
+    while l < len(low_arr) and h < len(high_arr):
+        if low_arr[l] <= high_arr[h] :
+            sorted_arr.append(low_arr[l])
+            ans.append(low_arr[l])
+            l += 1
+        else :
+            sorted_arr.append(high_arr[h])
+            ans.append(high_arr[h])
+            h += 1
+            
+    while l < len(low_arr) :
+        sorted_arr.append(low_arr[l])
+        ans.append(low_arr[l])
+        l += 1
+    
+    while h < len(high_arr):
+        sorted_arr.append(high_arr[h])
+        ans.append(high_arr[h])
+        h +=1
+        
+    return sorted_arr
 
-N = int(sys.stdin.readline())
-
-for _ in range(N):
-    s = sys.stdin.readline().strip()
-    print(isPalindrome(s, 0)[0], isPalindrome(s, 0)[1])
+ans = []
+N,K = map(int,input().split())
+merge_sort(list(map(int,input().split())))
+print(ans[K-1] if K <= len(ans) else -1)
+    
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
 # ---------------
