6a6a8d398f8b5879a28b97a01ccc8c8aaf6743d9
gwangbaekun <gwangbaekun@gmail.com>
2023-12-25 21:12:39 +0900

    별 찍기 10

--- python/test.py
+++ python/test.py
@@ -1,32 +1,31 @@
 import sys
 import time
-from collections import deque
 
 # 주석------------
-sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
-start = time.time()
+# sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
+# start = time.time()
 #----------------
 
 def func(n):
+
     if n == 1:
         return ['*']
-    
-    Stars = func(n // 3)
-    l = []
-    
-    for star in Stars:
-        l.append(star * 3)
-    for star in Stars:
-        l.append(star + ' ' * (n // 3) + star)
-    for star in Stars:
-        l.append(star * 3)
 
-    return l
+    Stars = func(n//3)
+    L = []
+    for star in Stars:
+        L.append(star * 3)
+    for star in Stars:
+        L.append(star + ' ' * (n // 3) + star)
+    for star in Stars:
+        L.append(star * 3)
+
+
+    return L
 
 N = int(sys.stdin.readline())
-
-print('\n'.join(func(N)))
+print("\n".join(func(N)))
 
 # 주석------------
-print(f"time : {time.time() - start:.5f} sec")
+# print(f"time : {time.time() - start:.5f} sec")
 # ---------------
