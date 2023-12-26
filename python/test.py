35f07e098df062526582f4a05e8dd5b33f12d24e
gwangbaekun <gwangbaekun@gmail.com>
2023-12-26 22:04:28 +0900

    하노이탑

--- python/test.py
+++ python/test.py
@@ -1,31 +1,26 @@
 import sys
 import time
+from collections import deque
 
 # 주석------------
-# sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
-# start = time.time()
+sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
+start = time.time()
 #----------------
 
-def func(n):
-
+def recv(n, start, end):
     if n == 1:
-        return ['*']
+        print(start, end)
+        return
+    
+    recv(n - 1, start, 6 - start - end)
+    print(start, end)
+    recv(n - 1, 6 - start - end, end)
 
-    Stars = func(n//3)
-    L = []
-    for star in Stars:
-        L.append(star * 3)
-    for star in Stars:
-        L.append(star + ' ' * (n // 3) + star)
-    for star in Stars:
-        L.append(star * 3)
-
-
-    return L
 
 N = int(sys.stdin.readline())
-print("\n".join(func(N)))
+print(2 ** N - 1)
+recv(N, 1, 3)
 
 # 주석------------
-# print(f"time : {time.time() - start:.5f} sec")
+print(f"time : {time.time() - start:.5f} sec")
 # ---------------
