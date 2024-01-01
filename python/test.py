356ac25aae8687dbc360cd6a0a2aac9216db5208
gwangbaekun <gwangbaekun@gmail.com>
2024-01-01 19:53:28 +0900

    연산자 끼워넣기

--- python/test.py
+++ python/test.py
@@ -5,21 +5,37 @@ from collections import deque
 # 주석------------
 sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
+input = sys.stdin.readline
 #----------------
 
-def recv(n, start, end):
-    if n == 1:
-        print(start, end)
+max_val = -1e9 - 1
+min_val = 1e9 + 1
+
+def cal(depth, result, plus, minus, multi, div):
+    global N, max_val, min_val
+    if depth == N:
+        max_val = max(max_val, result)
+        min_val = min(min_val, result)
         return
     
-    recv(n - 1, start, 6 - start - end)
-    print(start, end)
-    recv(n - 1, 6 - start - end, end)
-
+    if plus:
+        cal(depth+1, result+nums[depth], plus-1, minus, multi, div)
+    if minus:
+        cal(depth+1, result-nums[depth], plus, minus-1, multi, div)
+    if multi:
+        cal(depth+1, result*nums[depth], plus, minus, multi-1, div)
+    if div:
+        if result < 0:
+            cal(depth+1, -(-result//nums[depth]), plus, minus, multi, div-1)
+        else:
+            cal(depth+1, result//nums[depth], plus, minus, multi, div-1)
 
 N = int(sys.stdin.readline())
-print(2 ** N - 1)
-recv(N, 1, 3)
+nums = list(map(int, sys.stdin.readline().split()))
+plus, minus, multi, div = map(int, sys.stdin.readline().split())
+
+cal(1, nums[0], plus, minus, multi, div)
+print(int(max_val), int(min_val), sep='\n')
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
