6059a8286cc8e07bf9d78974f0531c8050f0af5e
gwangbaekun <gwangbaekun@gmail.com>
2024-01-09 15:21:27 +0900

    RGB 거리

--- python/test.py
+++ python/test.py
@@ -5,36 +5,19 @@ from collections import deque
 # 주석------------
 sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
 start = time.time()
-# input = sys.stdin.readline
+input = sys.stdin.readline
 #----------------
 
-def dfs(depth, curIdx):
-    global res
-    if depth == N // 2:
-        A = 0
-        B = 0
-        for i in range(N):
-            for j in range(N):
-                if visited[i] and visited[j]:
-                    A += board[i][j]
-                elif not visited[i] and not visited[j]:
-                    B += board[i][j]
-        res = min(res, abs(A - B))
-        return
-    for i in range(curIdx, N):
-        if not visited[i]:
-            visited[i] = 1
-            dfs(depth + 1, curIdx + 1)
-            visited[i] = 0
+N = int(input())
+A = [list(map(int, input().split()) ) for _ in range(N)]
+a = A
 
-N = int(sys.stdin.readline())
-board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-visited = [0 for _ in range(N)]
-INF = 2147000000
-res = INF
+for i in range(1, N):
+    a[i][0] = min(A[i-1][1], A[i-1][2]) + A[i][0]
+    a[i][1] = min(A[i-1][0], A[i-1][2]) + A[i][1]
+    a[i][2] = min(A[i-1][0], A[i-1][1]) + A[i][2]
 
-dfs(0, 0)
-print(res)
+print(min(a[N - 1]))    
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
