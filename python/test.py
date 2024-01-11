b75874ef3969fe425d46bef1416c0f4a675d9d1e
gwangbaekun <gwangbaekun@gmail.com>
2024-01-11 23:30:08 +0900

    녹색거탑

--- python/test.py
+++ python/test.py
@@ -2,24 +2,15 @@ import sys
 import time
 
 # 주석------------
-# sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
-# start = time.time()
+sys.stdin = open('/Users/home/Developer/study/Algorithm/python/python.txt', 'r')
+start = time.time()
 #----------------
 input = sys.stdin.readline
 #----------------
 
 N = int(input())
-dp = [0] * (N+1)
-
-for i in range(2, N+1):
-    dp[i] = dp[i-1] + 1
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-
-print(dp[N])
+print(2 ** N)
 
 # 주석------------
-# print(f"time : {time.time() - start:.5f} sec")
+print(f"time : {time.time() - start:.5f} sec")
 # ---------------
