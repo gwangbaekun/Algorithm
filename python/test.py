f9e8c68a56b33bf6aaddd125d8fe69d3a1e1f5f5
gwangbaekun <gwangbaekun@gmail.com>
2024-01-19 23:12:29 +0900

    베라의 패션

--- python/test.py
+++ python/test.py
@@ -2,32 +2,15 @@ import sys
 import time
 
 # 주석------------
-# sys.stdin = open('/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt', 'r')
-# start = time.time()
+sys.stdin = open('/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt', 'r')
+start = time.time()
 #----------------
 input = sys.stdin.readline
 #----------------
 
 N = int(input())
-dp = [[0 for _ in range(10)] for _ in range(N+1)]
-for i in range(1, 10):
-    dp[1][i] = 1
-
-MOD = 1000000000
-
-for i in range(2, N+1):
-    for j in range(10):
-        if j == 0:
-            dp[i][j] = dp[i-1][1]
-        elif j == 9:
-            dp[i][j] = dp[i-1][8]
-        else:
-            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
-
-
-
-print(sum(dp[N]) % MOD)
+print(N * N - N)
 
 # 주석------------
-# print(f"time : {time.time() - start:.5f} sec")
+print(f"time : {time.time() - start:.5f} sec")
 # ---------------
