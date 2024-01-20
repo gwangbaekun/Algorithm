9f5634cb033b486189d6e1324f0209011ed578d7
gwangbaekun <gwangbaekun@gmail.com>
2024-01-20 18:21:28 +0900

    나는야 포켓몬 마스터 이다솜

--- python/test.py
+++ python/test.py
@@ -8,8 +8,21 @@ start = time.time()
 input = sys.stdin.readline
 #----------------
 
-N = int(input())
-print(N * N - N)
+n, m = map(int, input().split())
+
+dict = {}
+
+for i in range(1, n + 1):
+    a = input().rstrip()
+    dict[i] = a
+    dict[a] = i
+
+for i in range(m):
+    quest = input().rstrip()
+    if quest.isdigit():
+        print(dict[int(quest)])
+    else:
+        print(dict[quest])
 
 # 주석------------
 print(f"time : {time.time() - start:.5f} sec")
