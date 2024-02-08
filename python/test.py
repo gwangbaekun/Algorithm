6788397d9a9487f7b21441b524b85f1b503ed401
gwangbaekun <gwangbaekun@gmail.com>
2024-02-08 20:22:26 +0900

    가장 긴 바이토닉 부분 수열

--- python/test.py
+++ python/test.py
@@ -2,25 +2,31 @@ import sys
 import time
 
 # 주석------------
-sys.stdin = open('/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt', 'r')
-start = time.time()
+# sys.stdin = open('/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt', 'r')
+# start = time.time()
 #----------------
 input = sys.stdin.readline
 #----------------
 
 N = int(input())
-arr = list(int(input()) for _ in range(N))
-dp = [[0,0,0] for _ in range(N)]
+nums = list(map(int, input().split()))
+reverse_nums = nums[::-1]
 
-dp[0] = [arr[0], 0, 0]
+increase = [0] * N
+decrease = [0] * N
 
-for i in range(1, N):
-    dp[i][0] = dp[i-1][2] + arr[i]
-    dp[i][1] = dp[i-1][0] + arr[i]
-    dp[i][2] = max(dp[i-1][0], dp[i-1][1], dp[i-1][2])
+for i in range(N):
+    for j in range(i):
+        if nums[j] < nums[i]:
+            increase[i] = max(increase[i], increase[j] + 1)
+        if reverse_nums[j] < reverse_nums[i]:
+            decrease[i] = max(decrease[i], decrease[j] + 1)
 
-print(max(dp[-1]))
+for i in range(N):
+    increase[i] = increase[i] + decrease[N - i - 1]
+
+print(max(increase) + 1)
 
 # 주석------------
-print(f"time : {time.time() - start:.5f} sec")
+# print(f"time : {time.time() - start:.5f} sec")
 # ---------------
