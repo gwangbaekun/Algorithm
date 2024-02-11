62ee4824c84ca6b2a49e00126b2f8d9611270166
gwangbaekun <gwangbaekun@gmail.com>
2024-02-11 13:20:18 +0900

    전깃줄

--- python/test.py
+++ python/test.py
@@ -9,23 +9,16 @@ input = sys.stdin.readline
 #----------------
 
 N = int(input())
-nums = list(map(int, input().split()))
-reverse_nums = nums[::-1]
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [1] * N
+arr.sort(key=lambda x: (x[0], x[1]))
 
-increase = [0] * N
-decrease = [0] * N
+for i in range(1, N):
+    for j in range(0, i):
+        if arr[j][1] < arr[i][1]:
+            dp[i] = max(dp[i], dp[j] + 1)
 
-for i in range(N):
-    for j in range(i):
-        if nums[j] < nums[i]:
-            increase[i] = max(increase[i], increase[j] + 1)
-        if reverse_nums[j] < reverse_nums[i]:
-            decrease[i] = max(decrease[i], decrease[j] + 1)
-
-for i in range(N):
-    increase[i] = increase[i] + decrease[N - i - 1]
-
-print(max(increase) + 1)
+print(N - max(dp))
 
 # 주석------------
 # print(f"time : {time.time() - start:.5f} sec")
