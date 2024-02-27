e38bcbf79fd6c3c51243c5e2ef2fec53b1fd8864
gwangbaekun <gwangbaekun@gmail.com>
2024-02-28 00:54:20 +0900

    LCS

--- python/test.py
+++ python/test.py
@@ -8,17 +8,17 @@ import time
 input = sys.stdin.readline
 #----------------
 
-N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-dp = [1] * N
-arr.sort(key=lambda x: (x[0], x[1]))
+char, char2 = input().strip(), input().strip()
+l1, l2 = len(char), len(char2)
+results = [[0] * (l2 + 1) for _ in range(l1 + 1)]
+for i in range(1, l1 + 1):
+    for j in range(1, l2 + 1):          
+        if char[i-1] == char2[j-1]:
+            results[i][j] = results[i-1][j-1] + 1
+        else:
+            results[i][j] = max(results[i-1][j], results[i][j-1])
 
-for i in range(1, N):
-    for j in range(0, i):
-        if arr[j][1] < arr[i][1]:
-            dp[i] = max(dp[i], dp[j] + 1)
-
-print(N - max(dp))
+print(results[-1][-1])
 
 # 주석------------
 # print(f"time : {time.time() - start:.5f} sec")
