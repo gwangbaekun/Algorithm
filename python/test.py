b073de647f42038480a6e6a9c646a8494818a988
gwangbaekun <gwangbaekun@gmail.com>
2024-12-18 22:10:31 +0900

    피보나치 수 (6)

--- python/test.py
+++ python/test.py
@@ -2,36 +2,36 @@ import sys
 import time
 
 # 주석------------
-sys.stdin = open('/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt', 'r')
-start = time.time()
+# sys.stdin = open('/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt', 'r')
+# start = time.time()
 #----------------
 input = sys.stdin.readline
 #----------------
 
-N, M = map(int, input().split())
-r = []
-visited = [False] * N
+N = int(input())
+matrix = [[1,1], [1,0]]
+mod = int(1E9 + 7)
 
-def dfs():
-    if len(r) == M:
-        print(' '.join(map(str, r)))
-        return
-    
-    for i in range(N):
-        if visited[i]:
-            continue
+def multiply(A, B):
+    return [
+        [(A[0][0] * B[0][0] + A[0][1] * B[1][0]) % mod, (A[0][0] * B[0][1] + A[0][1] * B[1][1]) % mod],
+        [(A[1][0] * B[0][0] + A[1][1] * B[1][0]) % mod, (A[1][0] * B[0][1] + A[1][1] * B[1][1]) % mod]
+    ]
 
-        visited[i] = True
-        r.append(i + 1)
-        dfs()
-        r.pop()
-        print(r)
-        print(visited)
-        visited[i] = False
+def power(A, n):
+    if n == 1:
+        return A
+    if n % 2 == 0:
+        route_arr = power(A, n // 2)
+        return multiply(route_arr, route_arr)
+    else:
+        return multiply(power(A, n - 1), A)
 
-dfs()
+def fibonacci(N):
+    return power(matrix, N)[0][1]
 
+print(fibonacci(N))
 
 # 주석------------
-print(f"time : {time.time() - start:.5f} sec")
+# print(f"time : {time.time() - start:.5f} sec")
 # ---------------
