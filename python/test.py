3d8a2a10c129d2edef8e135c110348a943c981f1
gwangbaekun <gwangbaekun@gmail.com>
2025-01-06 22:46:20 +0900

    꼬마 정민

--- python/test.py
+++ python/test.py
@@ -8,25 +8,9 @@ import time
 input = sys.stdin.readline
 #----------------
 
-N, M = map(int, input().split())
+N, M, K = map(int, input().split())
 
-visited = [False] * N
-
-def dfs(n, temp):
-    if len(temp) == M:
-        print(" ".join(map(str, temp)))
-        return
-    
-    for i in range(N):
-        if visited[i] == False:
-            temp.append(i + 1)
-            visited[i] = True
-            dfs(i + 1, temp)    
-            temp.pop()
-            visited[i] = False
-    
-
-dfs(0, [])
+print(N + M + K)
 
 
 # 주석------------
