e9af1941be357b73d5e57edb573b30bd0451efe1
gwangbaekun <gwangbaekun@gmail.com>
2025-01-06 22:50:06 +0900

    영수증

--- python/test.py
+++ python/test.py
@@ -2,17 +2,26 @@ import sys
 import time
 
 # 주석------------
-# sys.stdin = open('/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt', 'r')
-# start = time.time()
+sys.stdin = open('/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt', 'r')
+start = time.time()
 #----------------
 input = sys.stdin.readline
 #----------------
 
-N, M, K = map(int, input().split())
+X = int(input())
+N = int(input())
 
-print(N + M + K)
+sum = 0
 
+for _ in range(N):
+    a, b = map(int, input().split())
+    sum += a * b
+ 
+if sum == X:
+    print("Yes")
+else:
+    print("No")
 
 # 주석------------
-# print(f"time : {time.time() - start:.5f} sec")
+print(f"time : {time.time() - start:.5f} sec")
 # ---------------
