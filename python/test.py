085d87518c67c4b2630503b8b1478765d57e1838
gwangbaekun <gwangbaekun@gmail.com>
2025-02-23 06:08:44 +0900

    쿼드트리

--- python/test.py
+++ python/test.py
@@ -9,17 +9,40 @@ input = sys.stdin.readline
 # ----------------
 
 N = int(input())
-dis = list(map(int, input().split(" ")))
-gases = list(map(int, input().split(" ")))
+l = [list(input().strip()) for _ in range(N)]
 
-min_price = gases[0]
-sum_gas_price = 0
 
-for i in range(N - 1):
-    min_price = min(gases[i], min_price)
-    sum_gas_price += min_price * dis[i]
+def all_same(iterable):
+    first_value = iterable[0]
 
-print(sum_gas_price)
+    return (
+        first_value
+        if all(x == first_value for x in iterable)
+        else f"({"".join(iterable)})"
+    )
+
+
+def zip_with_2(list, depth):
+    if depth == 1:
+        return list[0][0]
+
+    zipped_list = []
+
+    for i in range(0, depth, 2):
+        zip_string = []
+        for j in range(0, depth, 2):
+            _iterable = [list[i][j], list[i][j + 1], list[i + 1][j], list[i + 1][j + 1]]
+            zip_string.append(all_same(_iterable))
+        zipped_list.append(zip_string)
+
+    return zip_with_2(zipped_list, int(depth / 2))
+
+
+if N == 1:
+    print(l[0][0])
+else:
+    result = zip_with_2(l, N)
+    print(result)
 
 # 주석------------
 # print(f"time : {time.time() - start:.5f} sec")
