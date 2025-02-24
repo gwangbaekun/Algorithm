5e71149effd0d497f828a4c3c545dbce1e86423f
gwangbaekun <gwangbaekun@gmail.com>
2025-02-24 12:25:17 +0900

    종이의 개수

--- python/test.py
+++ python/test.py
@@ -9,40 +9,47 @@ input = sys.stdin.readline
 # ----------------
 
 N = int(input())
-l = [list(input().strip()) for _ in range(N)]
+matrix = [list(map(int, input().split())) for _ in range(N)]
+
+papers = {-1: 0, 0: 0, 1: 0}
 
 
-def all_same(iterable):
-    first_value = iterable[0]
-
-    return (
-        first_value
-        if all(x == first_value for x in iterable)
-        else f"({"".join(iterable)})"
-    )
-
-
-def zip_with_2(list, depth):
-    if depth == 1:
-        return list[0][0]
-
-    zipped_list = []
-
-    for i in range(0, depth, 2):
-        zip_string = []
-        for j in range(0, depth, 2):
-            _iterable = [list[i][j], list[i][j + 1], list[i + 1][j], list[i + 1][j + 1]]
-            zip_string.append(all_same(_iterable))
-        zipped_list.append(zip_string)
-
-    return zip_with_2(zipped_list, int(depth / 2))
+def is_uniform(x, y, size):
+    first_value = matrix[x][y]
+    for i in range(x, x + size):
+        for j in range(y, y + size):
+            if matrix[i][j] != first_value:
+                return False
+    return True
 
 
-if N == 1:
-    print(l[0][0])
-else:
-    result = zip_with_2(l, N)
-    print(result)
+def trit_tree(x, y, size):
+    if is_uniform(x, y, size):
+        papers[matrix[x][y]] += 1
+        return
+
+    third = size // 3
+
+    trit_tree(x, y, third)
+    trit_tree(x + third, y, third)
+    trit_tree(x + 2 * third, y, third)
+
+    trit_tree(x, y + third, third)
+    trit_tree(x + third, y + third, third)
+    trit_tree(x + 2 * third, y + third, third)
+
+    trit_tree(x, y + 2 * third, third)
+    trit_tree(x + third, y + 2 * third, third)
+    trit_tree(x + 2 * third, y + 2 * third, third)
+
+    return
+
+
+trit_tree(0, 0, N)
+
+print(papers[-1])
+print(papers[0])
+print(papers[1])
 
 # 주석------------
 # print(f"time : {time.time() - start:.5f} sec")
