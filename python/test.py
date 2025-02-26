7c68e716c9f4a0d70ddae6fd1e5067e0234755c5
gwangbaekun <gwangbaekun@gmail.com>
2025-02-26 12:42:40 +0900

    가장 긴 증가하는 부분 수열 2

--- python/test.py
+++ python/test.py
@@ -8,17 +8,38 @@ import time
 input = sys.stdin.readline
 # ----------------
 
-N, M, K = map(int, input().split(" "))
-result = 1
-exp = M
-base = N
-while exp > 0:
-    if exp % 2 == 1:
-        result = result * base % K
-    base = base * base % K
-    exp //= 2
+N = int(input())
+l = list(map(int, input().split(" ")))
 
-print(result)
+result = [l[0]]
+
+
+def find_place(elem):
+    start = 0
+    end = len(result) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if elem == result[mid]:
+            return mid
+        elif elem < result[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return start
+
+
+for i in range(N):
+    if l[i] > result[-1]:
+        result.append(l[i])
+    else:
+        idx = find_place(l[i])
+        result[idx] = l[i]
+
+print(len(result))
+
 
 # 주석------------
 # print(f"time : {time.time() - start:.5f} sec")
