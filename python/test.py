068ba32c06c42c7535bd4cb88b6836bf28455cd1
gwangbaekun <gwangbaekun@gmail.com>
2025-02-27 13:51:48 +0900

    최소 힙

--- python/test.py
+++ python/test.py
@@ -3,8 +3,8 @@ import time
 import heapq
 
 # 주석------------
-sys.stdin = open("/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt", "r")
-start = time.time()
+# sys.stdin = open("/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt", "r")
+# start = time.time()
 # ----------------
 input = sys.stdin.readline
 # ----------------
@@ -16,15 +16,15 @@ for _ in range(N):
     n = int(input())
 
     if n > 0:
-        heapq.heappush(dq, -n)
+        heapq.heappush(dq, n)
 
     else:
         if len(dq) == 0:
             print(0)
         else:
-            print(-heapq.heappop(dq))
+            print(heapq.heappop(dq))
 
 
 # 주석------------
-print(f"time : {time.time() - start:.5f} sec")
+# print(f"time : {time.time() - start:.5f} sec")
 # ---------------
