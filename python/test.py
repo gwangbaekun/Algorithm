5a0ee6304282a391d11307c83b6aeabd61f81910
gwangbaekun <gwangbaekun@gmail.com>
2025-02-27 13:43:13 +0900

    최대 힙

--- python/test.py
+++ python/test.py
@@ -1,46 +1,30 @@
 import sys
 import time
+import heapq
 
 # 주석------------
-# sys.stdin = open("/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt", "r")
-# start = time.time()
+sys.stdin = open("/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt", "r")
+start = time.time()
 # ----------------
 input = sys.stdin.readline
 # ----------------
 
 N = int(input())
-l = list(map(int, input().split(" ")))
+dq = []
 
-result = [l[0]]
+for _ in range(N):
+    n = int(input())
 
+    if n > 0:
+        heapq.heappush(dq, -n)
 
-def find_place(elem):
-    start = 0
-    end = len(result) - 1
-
-    while start <= end:
-        mid = (start + end) // 2
-
-        if elem == result[mid]:
-            return mid
-        elif elem < result[mid]:
-            end = mid - 1
-        else:
-            start = mid + 1
-
-    return start
-
-
-for i in range(N):
-    if l[i] > result[-1]:
-        result.append(l[i])
     else:
-        idx = find_place(l[i])
-        result[idx] = l[i]
-
-print(len(result))
+        if len(dq) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(dq))
 
 
 # 주석------------
-# print(f"time : {time.time() - start:.5f} sec")
+print(f"time : {time.time() - start:.5f} sec")
 # ---------------
