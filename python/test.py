4071fd98049ce39ac2ea8c24f0d2de1a0f8d76c8
gwangbaekun <gwangbaekun@gmail.com>
2025-03-31 19:31:37 +0900

    ㅍㅏ일 합치기 DP pypy3 hard cocded

--- python/test.py
+++ python/test.py
@@ -1,31 +1,26 @@
 import sys
 import time
-import heapq
 
 # 주석------------
-sys.stdin = open("/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt", "r")
-start = time.time()
+# sys.stdin = open("/Users/jeyeolbaek/Developer/study/Algorithm/python/python.txt", "r")
+# start = time.time()
 # ----------------
 input = sys.stdin.readline
 # ----------------
 
 
 def min_merge_cost_dp(K, files):
-    # Prefix sum to quickly get the sum of any range
     prefix_sum = [0] * (K + 1)
     for i in range(K):
         prefix_sum[i + 1] = prefix_sum[i] + files[i]
 
-    # DP table
-    dp = [[0] * K for _ in range(K)]
+    dp = [[0 for i in range(K)] for j in range(K)]
 
-    # Solve for increasing subarray lengths
-    for length in range(2, K + 1):  # length = 2 to K
+    for length in range(2, K + 1):
         for i in range(K - length + 1):
             j = i + length - 1
             dp[i][j] = float("inf")
 
-            # Try all possible partitions
             for m in range(i, j):
                 cost = dp[i][m] + dp[m + 1][j] + (prefix_sum[j + 1] - prefix_sum[i])
                 dp[i][j] = min(dp[i][j], cost)
@@ -43,5 +38,5 @@ for _ in range(T):
     print(min_merge_cost_dp(K, files))
 
 # 주석------------
-print(f"time : {time.time() - start:.5f} sec")
+# print(f"time : {time.time() - start:.5f} sec")
 # ---------------
