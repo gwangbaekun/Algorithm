8bae317af9de7d727f73e8b6a570db5ab9c533a8
gwangbaekun <gwangbaekun@gmail.com>
2026-07-28 13:04:25 +0900

    3] 26. Remove Duplicates from Sorted Array

--- python/leetcode/leet_test.py
+++ python/leetcode/leet_test.py
@@ -1,3 +1,4 @@
+from itertools import count
 from pathlib import Path
 from typing import List
 
@@ -5,17 +6,24 @@ from test_runner import run_cases
 
 
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-        for el in nums:
-            if el != val:
-                nums[k] = el
-                k += 1
-
-        return k
+    def removeDuplicates(self, nums: List[int]) -> int:
+        idx = 1
+        count = 0
+        for i in range(1, len(nums)):
+            if nums[i-1] != nums[i]:
+                nums[idx] = nums[i]
+                idx += 1
+                count = 0
+            elif nums[i-1] == nums[i] and count == 0:
+                nums[idx] = nums[i]
+                idx += 1
+                count += 1
+            elif nums[i-1] == nums[i] and count >= 1:
+                count += 1
+        return idx, nums
 
 
 
 if __name__ == "__main__":
     case_file = Path(__file__).with_suffix(".txt")
-    run_cases(case_file, Solution().removeElement)
+    run_cases(case_file, Solution().removeDuplicates)
