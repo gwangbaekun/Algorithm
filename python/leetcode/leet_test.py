97869efaedc18b8a73ab77b5eb793d2efe547c5a
gwangbaekun <gwangbaekun@gmail.com>
2026-08-01 13:14:11 +0900

    121. Best Time to Buy and Sell Stock

--- python/leetcode/leet_test.py
+++ python/leetcode/leet_test.py
@@ -6,20 +6,15 @@ from test_runner import run_cases
 
 
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        n = len(nums)
-        k %= n
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float("inf")
+        best = 0
+        for p in prices:
+            min_price = min(min_price, p)
+            best = max(best, p - min_price)
+        
 
-        def reverse(l, r):
-            while l < r:
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r -= 1
-
-        reverse(0, n - 1)
-        reverse(0, k - 1)
-        reverse(k, n - 1)
 
 if __name__ == "__main__":
     case_file = Path(__file__).with_suffix(".txt")
-    run_cases(case_file, Solution().majorityElement)
+    run_cases(case_file, Solution().maxProfit)
